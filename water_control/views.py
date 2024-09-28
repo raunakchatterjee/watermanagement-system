31783635f1275ed64a9bd0184b1fa46769b7aa43
Raunak Chatterjee <154989940+raunakchatterjee@users.noreply.github.com>
2024-09-28 13:17:12 +0530

    Update views.py

--- water_control/views.py
+++ water_control/views.py
@@ -5,7 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 # Firebase configuration
 FIREBASE_URL = 'https://watermg-e82dd-default-rtdb.firebaseio.com/water.json'
 
-
 def index(request):
     # Fetch the current water percentage and pump status from Firebase
     response = requests.get(FIREBASE_URL)
